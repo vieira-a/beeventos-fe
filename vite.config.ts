648d1ed1fb7c1e3e60fc42efeb8c4aa90dcee783
Anderson Vieira <asvieira.br@gmail.com>
2024-05-29 09:14:38 -0300

    chore: configura vite como remote server

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,22 @@
-import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
-import path from "path";
-import { defineConfig } from "vite";
-import checker from "vite-plugin-checker";
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+import path from 'path';
+import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
 
 dotenv.config();
 
 export default defineConfig({
   plugins: [react(), checker({ typescript: true })],
   define: {
-    "process.env": process.env,
+    'process.env': process.env,
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
+  },
+  server: {
+    host: true,
   },
 });
